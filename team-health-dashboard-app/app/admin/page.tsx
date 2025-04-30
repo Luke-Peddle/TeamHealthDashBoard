521a74767529f4d4d33e3ea44bcbfc348d1c20c2
Luke-Peddle <luke@corsphere.com>
2025-04-30 13:52:16 -0230

    added the admin page which contains a link for the admin to go to the user management page

---
+++ team-health-dashboard-app/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h2>
+            Admin Page
+        </h2>
+        <a href="/admin/userManagement">Users</a>
+    </div>
+  )
+}
+
+export default page
