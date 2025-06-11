2362a2f929457beb3dd8248cc250281fd316eafa
Luke-Peddle <luke@corsphere.com>
2025-06-11 15:37:31 -0230

    added the KPI cards to the contributor dash baord

--- team-health-dashboard-app/pages/components/projects/KPICards/Metrics.tsx
+++ team-health-dashboard-app/pages/components/projects/KPICards/Metrics.tsx
@@ -14,70 +14,63 @@ interface MetricsProps {
    codeReviewMetrics: codeReview[]
    sprint_id: number
    pulseScores: pulse []
-
 }
 
 const Metrics: React.FC<MetricsProps> = ({ velocityMetric, onCallMetrics, codeReviewMetrics, pulseScores, sprint_id }) => {
 
-         
+    if (!velocityMetric || !onCallMetrics || !codeReviewMetrics) {
+        return <div>Loading metrics...</div>;
+    }
 
-     if (!velocityMetric || !onCallMetrics || !codeReviewMetrics) {
-       return <div>Loading metrics...</div>;
-   }
+    const currentVelocity = velocityMetric?.find(velocity => velocity.sprint_id === sprint_id);
 
-   const currentVelocity = velocityMetric?.find(velocity => velocity.sprint_id === sprint_id);
-
-  
     console.log("Code Review: " + codeReviewMetrics);
 
+    const currentOnCall = onCallMetrics?.filter(record => 
+         record.sprint_id === sprint_id
+    );
 
-   const currentOnCall = onCallMetrics?.filter(record => 
-        record.sprint_id === sprint_id
-   );
+    const currentCodeReviews = codeReviewMetrics?.filter(record => 
+         record.sprint_id === sprint_id
+    );
 
-   const currentCodeReviews = codeReviewMetrics?.filter(record => 
-        record.sprint_id === sprint_id
-   );
+    let incidentsCompleted = 0;
 
+    currentOnCall.forEach(oncall => {
+        incidentsCompleted += oncall.incidents_count
+    })
 
-   let incidentsCompleted = 0;
+    let prsReviewed = 0;
+    let reviewTime = 0;
 
-   currentOnCall.forEach(oncall => {
-       incidentsCompleted += oncall.incidents_count
-   })
+    currentCodeReviews.forEach(codeReview => {
+        prsReviewed += codeReview.prs_reviewed
+        console.log("res review in metric: " + prsReviewed)
+        reviewTime += Number(codeReview.avg_review_time_hours)
+        console.log("review time type: " + reviewTime)
 
-   let prsReviewed = 0;
-   let reviewTime = 0;
+        console.log("Code Review after process : " + JSON.stringify(codeReview))
 
-   currentCodeReviews.forEach(codeReview => {
-       prsReviewed += codeReview.prs_reviewed
-       console.log("res review in metric: " + prsReviewed)
-       reviewTime += Number(codeReview.avg_review_time_hours)
-       console.log("review time type: " + reviewTime)
+        console.log("res averge review time: " + typeof reviewTime)
+    })
 
-       console.log("Code Review after process : " + JSON.stringify(codeReview))
-
-       console.log("res averge review time: " + typeof reviewTime)
-   })
-
-   return (
-       <div className="mt-8">
-           <h2 className="text-xl font-semibold text-gray-800 mb-6">Sprint KPIs</h2>
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-               <VelocityKPICard velocity={currentVelocity} />
-               <OnCallKPICard 
-                   onCallIncidents={incidentsCompleted} 
-                   aveCompletedPerPerson={incidentsCompleted/2} 
-               />
-               <CodeReviewMetrics 
-                   prReviewed={prsReviewed} 
-                   avgReviewTime={reviewTime/2} 
-               />
-
-               <PulseKPICard pulses = {pulseScores} />
-           </div>
-       </div>
-   )
+    return (
+        <div className="mt-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-6">Sprint KPIs</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <VelocityKPICard velocity={currentVelocity} />
+                <OnCallKPICard 
+                    onCallIncidents={incidentsCompleted} 
+                    aveCompletedPerPerson={incidentsCompleted/2} 
+                />
+                <CodeReviewMetrics 
+                    prReviewed={prsReviewed} 
+                    avgReviewTime={reviewTime/2} 
+                />
+                <PulseKPICard pulses={pulseScores} />
+            </div>
+        </div>
+    )
 }
 
 export default Metrics
--- team-health-dashboard-app/pages/dashbaord/projects/[id]/index.tsx
+++ team-health-dashboard-app/pages/dashbaord/projects/[id]/index.tsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import PulseSurvey from '@/pages/components/PulseServey/PulseServey';
 import { useState } from 'react';
-import { X, BarChart3 } from 'lucide-react';
+import { X } from 'lucide-react';
+import Metrics from '@/pages/components/projects/KPICards/Metrics';
+import Charts from '@/pages/components/projects/Charts/Charts';
 
 const user = {
-    "user_id": 26,
+    "user_id": 24,
     "username": "SamSmithy", 
     "password": "Sam1234",
     "first_name": "Sam",
@@ -15,32 +17,163 @@ const user = {
     "role": "contributor"
 };
 
-export async function getServerSideProps(context) {
-    const { id } = context.params;
-
-    try {
-        const projectResponse = await axios.get(`http://localhost:4000/api/project/${id}`);
-        const project = projectResponse.data;
-
-        return {
-            props: {
-                project
-            }
-        };
-    }
-    catch (error) {
-        console.error('Server-side fetch error:', error);
-
-        return {
-            props: {
-                project: [],
-            }
-        };
+const fetchProject = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/project/${id}`);
+        return response.data;
     };
+
+    const fetchSprints = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/sprint/project/${id}`);
+        return response.data;
+    };
+
+    const fetchTeamMembers = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/users/teamMembers/${id}`);
+        return response.data;
+    };
+
+    const fetchNonTeamMembers = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/users/nonTeamMembers/${id}`);
+        return response.data;
+    };
+
+    const fetchVelocity = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/velocity/${id}`);
+        return response.data;
+    };
+
+    const fetchOnCall = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/oncall/${id}`);
+        return response.data;
+    };
+
+    const fetchCodeReview = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/codeReview/${id}`);
+        return response.data;
+    };
+
+    const fetchPulseSurvey = async (id) => {
+        const response = await axios.get(`http://localhost:4000/api/pulseSurvey/${id}`);
+        return response.data;
+    };
+
+ 
+
+export async function getServerSideProps(context) {
+
+    const { id } = context.params;
+       console.log(id);
+       
+       try {
+           const projectResponse = await axios.get(`http://localhost:4000/api/project/${id}`);
+           const sprintResponse = await axios.get(`http://localhost:4000/api/sprint/project/${id}`);
+           const teamMembersResponse = await axios.get(`http://localhost:4000/api/users/teamMembers/${id}`);
+           const nonTeamMembersResponse = await axios.get(`http://localhost:4000/api/users/nonTeamMembers/${id}`);
+           const velocityMetricResponse = await axios.get(`http://localhost:4000/api/velocity/${id}`);
+           const onCallMetricsResponse = await axios.get(`http://localhost:4000/api/oncall/${id}`);
+           const codeReviewMetricResponse = await axios.get(`http://localhost:4000/api/codeReview/${id}`);
+           const pulseResponse = await axios.get(`http://localhost:4000/api/pulseSurvey/${id}`)
+           
+   
+           const project = projectResponse.data;
+           const sprints = sprintResponse.data;
+           const teamMembers = teamMembersResponse.data;
+           const nonTeamMembers = nonTeamMembersResponse.data;
+           const velocityMetric = velocityMetricResponse.data;
+           const onCallMatrics = onCallMetricsResponse.data;
+           const codeReviewMatrics = codeReviewMetricResponse.data;
+           const pulseSurvey = pulseResponse.data;
+           
+           console.log("pulse Surveys data: " +  pulseResponse.data);
+           
+           return { 
+               props: { 
+                   project, 
+                   sprints, 
+                   teamMembers, 
+                   nonTeamMembers,
+                   projectId: id,
+                   velocityMetric,
+                   onCallMatrics,
+                   codeReviewMatrics,
+                   pulseSurvey
+               } 
+           };
+       } catch (error) {
+           console.error('Server-side fetch error:', error);
+           return { 
+               props: { 
+                   project: {}, 
+                   sprints: [], 
+                   teamMembers: [], 
+                   nonTeamMembers: [],
+                   projectId: id,
+                   velocityMetric: [],
+                   onCallMatrics: [],
+                   codeReviewMatrics: [],
+                   pulseSurvey: []
+               } 
+           };
+       }
 }
 
-const Index = ({ project }) => {
+const Index = ({ project: initialProject, sprints: initialSprints, teamMembers: initialTeamMembers, nonTeamMembers: initialNonTeamMembers, projectId, velocityMetric: initialVelocityMetric, onCallMatrics: initialOnCallMetric, codeReview: initalCodeReview, pulseSurvey: initialPulseSurvey }) => {
+   
     const [openPulseSurvey, setPulseSurvey] = useState(false);
+
+    const { data: project, isLoading: projectLoading } = useQuery({
+        queryKey: ['project', projectId],
+        queryFn: () => fetchProject(projectId),
+        initialData: initialProject,
+        staleTime: 2 * 60 * 1000, 
+    });
+
+    const { data: sprints } = useQuery({
+        queryKey: ['sprints', projectId],
+        queryFn: () => fetchSprints(projectId),
+        initialData: initialSprints,
+        staleTime: 30 * 1000, 
+    });
+
+    const { data: teamMembers } = useQuery({
+        queryKey: ['teamMembers', projectId],
+        queryFn: () => fetchTeamMembers(projectId),
+        initialData: initialTeamMembers,
+        staleTime: 5 * 60 * 1000,
+    });
+
+    const { data: nonTeamMembers } = useQuery({
+        queryKey: ['nonTeamMembers', projectId],
+        queryFn: () => fetchNonTeamMembers(projectId),
+        initialData: initialNonTeamMembers,
+        staleTime: 5 * 60 * 1000, 
+    });
+
+    const { data: velocityMetric } = useQuery({
+        queryKey: ['velocityMetrics', projectId],
+        queryFn: () => fetchVelocity(projectId),
+        initialData: initialVelocityMetric,
+        staleTime: 5 * 60 * 1000, 
+    });
+
+     const { data: onCallMatrics } = useQuery({
+        queryKey: ['onCallMatrics', projectId],
+        queryFn: () => fetchOnCall(projectId),
+        initialData: initialOnCallMetric,
+        staleTime: 5 * 60 * 1000, 
+    });
+    const { data: codeReviewMatrics } = useQuery({
+        queryKey: ['codeReviewMetrics', projectId],
+        queryFn: () => fetchCodeReview(projectId),
+        initialData: initalCodeReview,
+        staleTime: 5 * 60 * 1000, 
+    });
+    const { data: pulseSurvey } = useQuery({
+        queryKey: ['pulseSurvey', projectId],
+        queryFn: () => fetchPulseSurvey(projectId),
+        initialData: initialPulseSurvey,
+        staleTime: 5 * 60 * 1000, 
+    })
 
     const handleSurvey = () => {
         setPulseSurvey(true);
@@ -48,6 +181,40 @@ const Index = ({ project }) => {
 
     const handleCloseSurvey = () => {
         setPulseSurvey(false)
+    }
+
+    const getCurrentSprint = () =>{
+         if (!sprints || sprints.length === 0) return null;
+
+        const today = new Date()
+
+        today.setHours(0, 0, 0, 0);
+
+        const currentSprint = sprints.find(sprint =>{
+
+            console.log("Sprint in find: " + JSON.stringify(sprint))
+            console.log('today: ' + today)
+
+            const startDate = new Date(sprint.start_date);
+            const endDate = new Date(sprint.end_date);
+
+            startDate.setHours(0, 0, 0, 0);
+            endDate.setHours(23, 59, 59, 999);
+        
+            return today >= startDate && today <= endDate;
+        });
+
+        console.log("Current sprint in function: " + currentSprint)
+
+        return currentSprint || null;
+    }
+
+    let currentSprint = getCurrentSprint();
+
+
+    if(!currentSprint && sprints.length > 0){
+        currentSprint = sprints[0]
+
     }
 
     return (
@@ -80,10 +247,46 @@ const Index = ({ project }) => {
                             <div className="p-6 pt-12">
                                 <PulseSurvey project={project} user={user} />
                             </div>
+                             
                         </div>
                     </div>
                 </div>
             )}
+
+             <div className="px-6 mb-8">
+                    <div className="bg-white rounded-lg shadow-md p-6">
+                        <div className="mb-6">
+                            <h2 className="text-xl font-semibold text-gray-800 mb-2">
+                                Current Sprint: {currentSprint?.name || 'No active sprint'}
+                            </h2>
+                        </div>
+                        
+                        {currentSprint && (
+                            <Metrics 
+                                velocityMetric={velocityMetric} 
+                                onCallMetrics={onCallMatrics} 
+                                codeReviewMetrics={codeReviewMatrics} 
+                                pulseScores={pulseSurvey}
+                                sprint_id={currentSprint.id} 
+                            />
+                        )}
+                    </div>
+                </div>
+
+                 <div className="px-6 mb-8">
+                    <div className="bg-white rounded-lg shadow-md p-6">
+                        
+                        <Charts 
+                            velocityMetrics={velocityMetric} 
+                            incidents={onCallMatrics} 
+                            codeReview={codeReviewMatrics} 
+                            users={teamMembers} 
+                            sprints={sprints} 
+                            pulseSurveys={pulseSurvey}
+                            userRole = {user.role}
+                        />
+                    </div>
+                </div>
         </div>
     )
 }
